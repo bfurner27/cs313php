b723ef28344e81917791275e9bb5be0ae220e2eb
Ben Furner <bfurner27@yahoo.com>
2015-05-02 20:24:12 -0600

    made my homepage, added a picture and also another page to link to the home page. Also greatly improved the style and added some javascript

---
+++ index.js
@@ -0,0 +1,23 @@
+function showLinkAssignment() {
+	var doc = document.getElementById("assignPage");
+	doc.style.backgroundColor = "#E8E896"
+	doc.style.color = "#000032";
+}
+
+function reverseShowLink () {
+	var doc = document.getElementById("assignPage");
+	doc.style.backgroundColor = "#000032"
+	doc.style.color =  "#E8E896";
+}
+
+function showLinkAbout() {
+	var doc = document.getElementById("about");
+	doc.style.backgroundColor = "#E8E896"
+	doc.style.color = "#000032";
+}
+
+function reverseShowLinkAbout () {
+	var doc = document.getElementById("about");
+	doc.style.backgroundColor = "#000032"
+	doc.style.color =  "#E8E896";
+}
